dacde8b500485e3d7046f8fc16aa6dab50867093
fbara <fbara1@yahoo.com>
2021-05-29 18:09:57 -0500

    Start part 2: First Cell and Section.

---
+++ Airbnb Home/Views/HeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderView.swift
+//  Airbnb Home
+//
+//  Created by Frank Bara on 5/29/21.
+//
+
+import Anchorage
+import UIKit
+
+class HeaderView: ProgrammmaticView {
+    override func configure() {
+        backgroundColor = .systemTeal
+    }
+}
